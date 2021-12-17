4f156fba0fb023ca3834e8dc1f7cc4644df6a75e
frgryr2001 <nhanle.lx@gmail.com>
2021-12-17 16:04:23 +0700

    Update : layout AddReminder and render event click Time,Date ver 2

--- app/src/main/java/com/example/myapplication/AddReminder.java
+++ app/src/main/java/com/example/myapplication/AddReminder.java
@@ -1,29 +1,36 @@
 package com.example.myapplication;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
 import android.widget.Spinner;
 import android.widget.TableRow;
 import android.widget.TextView;
 import android.widget.TimePicker;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 public class AddReminder extends AppCompatActivity {
-    TextView tvTime;
+    TextView tvTime,tvDate;
     Spinner spinner;
-    TableRow rowTime;
+    TableRow rowTime,rowDate;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -72,7 +79,42 @@ public class AddReminder extends AppCompatActivity {
         final ArrayAdapter<String> spinnerArrayAdapter = new ArrayAdapter<>(this, R.layout.support_simple_spinner_dropdown_item, plantsList);
         spinnerArrayAdapter.setDropDownViewResource(R.layout.spinner_value);
         spinner.setAdapter(spinnerArrayAdapter);
+        // end
+
+        String currentDate = new SimpleDateFormat("dd-MM-yyyy", Locale.getDefault()).format(new Date());
+        tvDate = findViewById(R.id.tvDate);
+        rowDate = findViewById(R.id.rowDate);
+        tvDate.setText(currentDate);
+        rowDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                chonNgay();
+            }
+        });
+
     }
+
+
+
+
+
+    // function chon ngày
+    private void chonNgay(){
+        final Calendar calendar = Calendar.getInstance();
+        int ngay = calendar.get(Calendar.DATE);
+        int thang = calendar.get(Calendar.MONTH);
+        int nam = calendar.get(Calendar.YEAR);
+        DatePickerDialog datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+                calendar.set(i,i1,i2);
+                SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd-MM-yyyy");
+                tvDate.setText(simpleDateFormat.format(calendar.getTime()));
+            }
+        },nam,thang,ngay);
+        datePickerDialog.show();
+    }
+//   // end
 
     // Tạo nút Back về Home
     public boolean onOptionsItemSelected(MenuItem item){
@@ -80,12 +122,15 @@ public class AddReminder extends AppCompatActivity {
             case android.R.id.home:
                 finish();
                 return true;
+            case R.id.menuSave:
+                // Something code
         }
         return super.onOptionsItemSelected(item);
     }
 
     public boolean onCreateOptionsMenu(Menu menu) {
-        return true;
+        getMenuInflater().inflate(R.menu.save, menu);
+        return super.onCreateOptionsMenu(menu);
     }
     // end
 }
